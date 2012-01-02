9dc923d54879e9649d48f2ab9b5894bb36e20214
Zach Hirsch <zhirsch@umich.edu>
2012-01-02 07:19:12 -0800

    It works.

--- lib/light3/game.rb
+++ lib/light3/game.rb
@@ -2,12 +2,66 @@
 #
 # The light3 game driver.
 
-module Light3
+require 'light3/rules'
 
+module Light3
   # Runs a game of light3.
+  #
   class Game
+    include Rules
+
+    def initialize
+      @board = [[nil, nil, nil], [nil, nil, nil], [nil, nil, nil]]
+      @current_player = nil
+    end
+
+    # Plays the game. Doesn't return until the game has been won.
+    #
     def play
+      until game_over
+        start_new_turn
+        show_board
+        make_move @current_player
+      end
+      show_board
+      puts "#{game_over} wins!"
+    end
+
+    private
+
+    # The sequence of squares that form a winning combination.
+    #
+    WinningPoints =
+      [
+       # Rows
+       [ [0, 0], [0, 1], [0, 2]],
+       [ [1, 0], [1, 1], [2, 2]],
+       [ [2, 0], [2, 1], [1, 2]],
+       # Columns
+       [ [0, 0], [1, 0], [2, 0]],
+       [ [0, 1], [1, 1], [2, 1]],
+       [ [0, 2], [1, 2], [2, 2]],
+       # Diagonals
+       [ [0, 0], [1, 1], [2, 2]],
+       [ [0, 2], [1, 1], [2, 0]],
+      ]
+
+    def game_over
+      winner = WinningPoints.map do |points|
+        squares = points.map { |row, col| @board[row][col] }
+        squares.reduce do |memo, player|
+          if memo == player
+            memo
+          end
+        end
+      end
+      if winner.any?
+        winner.compact
+      end
+    end
+
+    def start_new_turn
+      @current_player = (@current_player == :player1) ? :player2 : :player1
     end
   end
-
 end
