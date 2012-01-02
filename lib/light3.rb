996e3cb4c5d80dffcc119a36c41f110648be84a4
Zach Hirsch <zhirsch@umich.edu>
2012-01-01 21:01:57 -0800

    Add Light3::Game class.

--- lib/light3.rb
+++ lib/light3.rb
@@ -2,5 +2,9 @@
 #
 # This is the entry point for light3.
 
+require 'light3/game'
+
 if __FILE__ == $0
+  game = Light3::Game.new
+  game.play
 end
---
+++ lib/light3/game.rb
@@ -0,0 +1,13 @@
+# Light3::Game
+#
+# The light3 game driver.
+
+module Light3
+
+  # Runs a game of light3.
+  class Game
+    def play
+    end
+  end
+
+end
